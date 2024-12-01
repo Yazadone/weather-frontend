bcf71be7f86cfc6231a4fac11d4cea449055234b
Adnanzahid12 <akadaniz123@gmail.com>
2024-12-01 11:41:08 -0500

    Choiyon's Code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import SearchBar from "./Components/SearchBar/searchBar";
 import { DataProvider } from "./Components/DataProvider/dataProvider";
-
+import AirDetailsComponent from "./Components/AirDetailComponents/AirDetailComponents";
+import SunDetailsComponent from "./Components/SunDetailsComponent/SunDetailsComponent";
 
 function App() {
   return (
     <DataProvider>
       <SearchBar/>
+      <AirDetailsComponent/>
+      <SunDetailsComponent/>
     </DataProvider>
   );
 }
