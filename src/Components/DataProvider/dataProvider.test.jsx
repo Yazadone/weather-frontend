03f232228f37f397989c2ba4069fe13fc591ad39
Choiyon Chakraborty <84105362+cchoiyon@users.noreply.github.com>
2024-12-03 22:56:23 -0500

    Create dataProvider.test.jsx
    test created data provider

---
+++ src/Components/DataProvider/dataProvider.test.jsx
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect } from 'react';
+import { render, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { DataProvider, DataContext } from './dataProvider';
+
+global.fetch = jest.fn();
+
+beforeEach(() => {
+  jest.spyOn(console, 'log').mockImplementation(() => { });
+});
+
+afterEach(() => {
+  console.log.mockRestore();
+});
+
+test('logs an error message if the API request fails', async () => {
+  fetch.mockImplementationOnce(() => Promise.reject(new Error('API is down')));
+
+  const TestComponent = () => {
+    const { setsearchData } = useContext(DataContext);
+
+    useEffect(() => {
+      setsearchData('test city');
+    }, [setsearchData]);
+
+    return <div>Test Component</div>;
+  };
+
+  render(
+    <DataProvider>
+      <TestComponent />
+    </DataProvider>
+  );
+
+  await waitFor(() => {
+    expect(console.log).toHaveBeenCalledWith(new Error('API is down'));
+  });
+});
